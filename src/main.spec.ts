3a4d1d664e0913c040b428286d8d1f625d65f77f
Tom Marien <tommarien@gmail.com>
2021-07-28 14:38:34 +0200

    Added tap as test runner

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,3 +1,6 @@
-test('it should not be this kind of rubbish', () => {
-  expect(true).toBeFalsy();
+import tap from 'tap';
+
+tap.test('it should not be this kind of rubbish', (t) => {
+  t.equal(true, false);
+  t.end();
 });
