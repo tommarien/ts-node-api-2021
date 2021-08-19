c07c9058b9ae06b1b383040e06481ddb9514d8ec
Tom Marien <tommarien@gmail.com>
2021-08-19 17:30:03 +0200

    chore: setup migrations

---
+++ migrations/sample-migration.ts
@@ -0,0 +1,9 @@
+import { Db, MongoClient } from 'mongodb';
+
+export async function up(db: Db, client: MongoClient): Promise<void> {
+  // TODO write your migration here.
+}
+
+export async function down(db: Db, client: MongoClient): Promise<void> {
+  // TODO write your migration here.
+}
