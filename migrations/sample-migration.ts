6f236db973b5283e394c2422e37158bba598a79f
Tom Marien <tommarien@gmail.com>
2022-09-11 22:04:56 +0200

    chore: remove migration change leftover

--- migrations/sample-migration.ts
+++
@@ -1,9 +0,0 @@
-import { Db, MongoClient } from 'mongodb';
-
-export async function up(db: Db, client: MongoClient): Promise<void> {
-  // TODO write your migration here.
-}
-
-export async function down(db: Db, client: MongoClient): Promise<void> {
-  // TODO write your migration here.
-}
